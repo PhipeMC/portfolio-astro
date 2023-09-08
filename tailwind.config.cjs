1bf6b0caa126741865aa16c44ed0f16bb12e1c64
PhipeMC <phipe.mcbukkit@gmail.com>
2023-09-07 18:08:25 -0600

    => Actualizacion de Astro 2 -> 3.0 => Implementacion de pages/about.astro => Estilos modificados

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import preline from 'preline/plugin.js';
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./public/**/*.astro',
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'node_modules/preline/dist/*.js',
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		preline,
+	],
 }
